83b05ac8b8357dfea54e5315681e3ceb04c9f940
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-05-16 17:00:52 +0200

    Eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,67 +1,28 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-    {
-        ignores: ["components/ui/**/*"],
-    },
+export default [
     ...compat.extends(
         "next/core-web-vitals",
         "next/typescript",
         "standard",
+        "plugin:tailwindcss/recommended",
         "prettier"
-        // "plugin:tailwindcss/recommended"
     ),
     {
-        rules: {
-            "import/order": [
-                "error",
-                {
-                    groups: [
-                        "builtin",
-                        "external",
-                        "internal",
-                        ["parent", "sibling"],
-                        "index",
-                        "object",
-                    ],
-
-                    "newlines-between": "always",
-
-                    pathGroups: [
-                        {
-                            pattern: "@app/**",
-                            group: "external",
-                            position: "after",
-                        },
-                    ],
-
-                    pathGroupsExcludedImportTypes: ["builtin"],
-
-                    alphabetize: {
-                        order: "asc",
-                        caseInsensitive: true,
-                    },
-                },
-            ],
-            "comma-dangle": "off",
-        },
-    },
-    {
-        files: ["**/*.ts", "**/*.tsx"],
-
         rules: {
             "no-undef": "off",
         },
     },
 ];
-
-export default eslintConfig;
