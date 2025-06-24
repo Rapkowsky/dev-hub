410ffb239843e085d03abeda834e1c3ff2baa68f
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-06-24 20:20:25 +0200

    Implement User Answers Tab

--- components/cards/answer-card.tsx
+++ components/cards/answer-card.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 import ROUTES from "@/constants/routes";
-import { getTimeStamp } from "@/lib/utils";
-
-import UserAvatar from "../user-avatar";
-import { Preview } from "../editor/preview";
-import { Suspense } from "react";
-import Votes from "../votes/votes";
 import { hasVoted } from "@/lib/actions/vote.action";
+import { cn, getTimeStamp } from "@/lib/utils";
+import { Preview } from "../editor/preview";
+import UserAvatar from "../user-avatar";
+import Votes from "../votes/votes";
+
+interface Props extends Answer {
+    containerClasses?: string;
+    showReadMore?: boolean;
+}
 
 const AnswerCard = ({
     _id,
@@ -16,14 +20,20 @@ const AnswerCard = ({
     createdAt,
     upvotes,
     downvotes,
-}: Answer) => {
+    question,
+    containerClasses,
+    showReadMore = false,
+}: Props) => {
     const hasVotedPromise = hasVoted({
-        targetType: "answer",
         targetId: _id,
+        targetType: "answer",
     });
+
     return (
-        <article className="light-border border-b py-10">
-            <span id={JSON.stringify(_id)} className="hash-span" />
+        <article
+            className={cn("light-border border-b py-10", containerClasses)}
+        >
+            <span id={`answer-${_id}`} className="hash-span" />
 
             <div className="mb-5 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
                 <div className="flex flex-1 items-start gap-1 sm:items-center">
@@ -63,6 +73,15 @@ const AnswerCard = ({
             </div>
 
             <Preview content={content} />
+
+            {showReadMore && (
+                <Link
+                    href={`/questions/${question}#answer-${_id}`}
+                    className="body-semibold font-space-grotesk text-primary-500 relative z-10"
+                >
+                    <p className="mt-1">Read more...</p>
+                </Link>
+            )}
         </article>
     );
 };
