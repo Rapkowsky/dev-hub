53f7540112b707beb221f9fdd37281ffd50955f1
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-06-26 21:51:25 +0200

    Improve input style in dark mode

--- components/search/global-search.tsx
+++ components/search/global-search.tsx
@@ -67,7 +67,7 @@ const GlobalSearch = () => {
             className="relative w-full max-w-[600px] max-lg:hidden"
             ref={searchContainerRef}
         >
-            <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4">
+            <div className="background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-1 rounded-xl px-4 dark:bg-black">
                 <Image
                     src="/icons/search.svg"
                     alt="search"
