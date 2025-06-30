d8df6b22aa555aa5fd2b84905eb687b0011095d5
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-06-30 13:59:03 +0200

    Add loading skeleton to profile edit page

---
+++ app/(root)/profile/edit/loading.tsx
@@ -0,0 +1,24 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+const Loading = () => {
+    return (
+        <section>
+            <h1 className="h1-bold text-dark100_light900 mb-10">
+                Edit Profile
+            </h1>
+
+            {[1, 2, 3, 4].map((item) => (
+                <div key={item} className="mb-[36px]">
+                    <Skeleton className="mb-[12px] h-6 w-[90px]" />
+                    <Skeleton className="h-[50px] w-full" />
+                </div>
+            ))}
+
+            <Skeleton className="mb-[12px] h-6 w-[90px]" />
+            <Skeleton className="h-[130px] w-full" />
+            <Skeleton className="mt-[60px] h-[36px] w-[79px] ml-auto" />
+        </section>
+    );
+};
+
+export default Loading;
