bf07c19135cf10bd3f94f82f27f90c31fce2e72c
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-06-30 12:22:40 +0200

    Improve loading skeleton for community

--- app/(root)/community/loading.tsx
+++ app/(root)/community/loading.tsx
@@ -6,12 +6,12 @@ const Loading = () => {
             <h1 className="h1-bold text-dark100_light900">All Users</h1>
 
             <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-                <Skeleton className="h-14 flex-1" />
-                <Skeleton className="h-14 w-28" />
+                <Skeleton className="h-[50px] w-full" />
+                <Skeleton className="h-[40px] w-full max-w-[154px] sm:h-[50px]" />
             </div>
 
             <div className="mt-12 flex flex-wrap gap-5">
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+                {[1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
                     <Skeleton
                         key={item}
                         className="xs:w-[230px] h-60 w-full rounded-2xl"
